27485cb5cb7224abb8af54ab80e9602609fbe913
VaibhaviRaut <46416596+VaibhaviRaut@users.noreply.github.com>
2020-05-15 14:57:43 -0400

    uploading util files

---
+++ ConstantValues.java
@@ -0,0 +1,47 @@
+package mediSmart.Util;
+
+public class ConstantValues {
+
+    public static final String EMAIL="email"; 
+    public static final String PASSWORD="password"; 
+    public static final String NAME="name";
+    public static final String MOBILE="mobile";
+    public static final String CITY="city";
+    public static final String USERINFO="UserInfo";
+    public static final String INCREMENT="increment";
+
+
+    public static final String RESULT="result"; 
+    public static final String SUCCESS="success"; 
+    public static final String FAIL="fail"; 
+    public static final String ADDED="Added"; 
+    public static final String UPDATED="Updated"; 
+    public static final String EXIST="Exist"; 
+    public static final String JARRAY="jarray";
+    public static final String JARRAYOUTER="jarrayOuter";
+    public static final String JARRAYINNER="jarrayInner";
+    public static final String OUTERJOBJ="outerJobj";
+
+    public static final String USERID="userId"; 
+
+
+
+
+    public static final String DELETED="Deleted";
+
+   
+    public static final String EMAIL_EXISTS="Email Exists";
+    public static final String LOGIN_FAIL="Login Failed";
+
+    
+    public static final String APPSERVCIE="/AppService";
+
+    public static final String REGISTRATION="/Registration";
+    public static final String LOGIN="/Login";
+  
+
+
+
+
+
+}
